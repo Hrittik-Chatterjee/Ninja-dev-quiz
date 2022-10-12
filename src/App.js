f5196308ea2274a3175e564b4ffcb4c8bf3f24f2
Hrittik-Chatterjee <@hrittik0chatterjee@gmail.com>
2022-10-12 18:10:51 +0600

    error added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
 
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Header from './Components/Header/Header';
+
 import About from './Components/About/About';
 import Statics from './Components/Statics/Statics';
 import Faq from './Components/Faq/Faq';
+
+import Layout from './Components/Layout/Layout';
 import Home from './Components/Home/Home';
+import Error from './Components/Error/Error';
 
 function App() {
   const router =createBrowserRouter([
       {
         path:'/',
-        element:<Home></Home>
-      },
-      {
-        path:'/about',
-        element:<About></About>
-      },
-      {
-        path:'/statics',
-        element:<Statics></Statics>
-      },
-      {
-        path:'/faq',
-        element:<Faq></Faq>
+        element:<Layout></Layout>,
+        errorElement: <Error></Error> ,
+        children :[
+          {
+            path:'/',
+            element:<Home></Home>
+          },
+          {
+            path:'/home',
+            element:<Home></Home>
+          },
+          {
+            path:'/about',
+            element:<About></About>
+          },
+          {
+            path:'/statics',
+            element:<Statics></Statics>
+          },
+          {
+            path:'/faq',
+            element:<Faq></Faq>
+          }
+        ]
       }
+      
   ])
   return (
     <div className="App">
-      <Header></Header>
+      
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
