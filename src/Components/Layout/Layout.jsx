a4052ed9211caea367d2a1768b1454f16228a75c
Hrittik-Chatterjee <hrittik0chatterjee@gmail.com>
2024-12-01 13:57:18 +0600

    black issue fixed with loaded

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,16 +1,35 @@
-import React, { createContext } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { Outlet, useLoaderData } from "react-router-dom";
 import Header from "../Header/Header";
+import { DotLoader } from "react-spinners";
 
 export const TopicContext = createContext([]);
 
 const Layout = () => {
-  const topics = useLoaderData();
+  const [topics, setTopics] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchedTopics = useLoaderData();
+
+  useEffect(() => {
+    if (fetchedTopics && fetchedTopics.length > 0) {
+      setTopics(fetchedTopics);
+      setLoading(false);
+    }
+  }, [fetchedTopics]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <DotLoader color="#C71585" />
+      </div>
+    );
+  }
 
   return (
     <TopicContext.Provider value={topics}>
-      <Header></Header>
-      <Outlet></Outlet>
+      <Header />
+      <Outlet />
     </TopicContext.Provider>
   );
 };
